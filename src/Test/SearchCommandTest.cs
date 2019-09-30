0794dff5169451a711337654b036f3524ae310a1
Wolfgang Berger <wolfgang.berger@viperfisch.de>
2019-09-30 22:15:09 +0200

    Tests were searching in old solution

--- src/Test/SearchCommandTest.cs
+++ src/Test/SearchCommandTest.cs
@@ -9,18 +9,18 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Aspenlaub.Net.GitHub.CSharp.PureSearch.Test {
     [TestClass]
     public class SearchCommandTest {
-        private readonly IFolder vCSharpFolder;
+        private readonly IFolder vGitHubFolder;
 
         public SearchCommandTest() {
             var componentProvider = new ComponentProvider();
             var errorsAndInfos = new ErrorsAndInfos();
-            vCSharpFolder = componentProvider.FolderResolver.Resolve(@"$(CSharp)", errorsAndInfos);
+            vGitHubFolder = componentProvider.FolderResolver.Resolve(@"$(GitHub)", errorsAndInfos);
             Assert.IsFalse(errorsAndInfos.AnyErrors(), errorsAndInfos.ErrorsToString());
         }
 
         [TestMethod]
         public async Task CannotSearchIfFolderDoesNotExist() {
-            var searchFolder = new FakeSearchFolder { SearchInFolder = vCSharpFolder.FullName + @"\PureSearch\PureSearch.Test\" };
+            var searchFolder = new FakeSearchFolder { SearchInFolder = vGitHubFolder.FullName + @"\PureSearch\PureSearch.Test\" };
             var searchArguments = new FakeSearchArguments { MatchCase = false, NameContains = "Fake", SearchFor = "PureSearch", TextThatFollows = "", TextThatDoesNotFollow = "" };
             var searchCommand = new SearchCommand(searchFolder, searchArguments);
             var context = new FakeApplicationCommandExecutionContext();
@@ -30,7 +30,7 @@ namespace Aspenlaub.Net.GitHub.CSharp.PureSearch.Test {
 
         [TestMethod]
         public async Task CanSearch() {
-            var searchFolder = new FakeSearchFolder { SearchInFolder = vCSharpFolder.FullName + @"\PureSearch\Test\" };
+            var searchFolder = new FakeSearchFolder { SearchInFolder = vGitHubFolder.FullName + @"\PureSearch\src\Test\" };
             var searchArguments = new FakeSearchArguments { MatchCase = false, NameContains = "Fake", SearchFor = "PureSearch", TextThatFollows = "", TextThatDoesNotFollow = "" };
             var searchCommand = new SearchCommand(searchFolder, searchArguments);
             var context = new FakeApplicationCommandExecutionContext();
@@ -41,7 +41,7 @@ namespace Aspenlaub.Net.GitHub.CSharp.PureSearch.Test {
 
         [TestMethod]
         public async Task CanSearchOnFollowingLine() {
-            var searchFolder = new FakeSearchFolder() { SearchInFolder = vCSharpFolder.FullName + @"\PureSearch\Test\" };
+            var searchFolder = new FakeSearchFolder() { SearchInFolder = vGitHubFolder.FullName + @"\PureSearch\src\Test\" };
             var searchArguments = new FakeSearchArguments() { MatchCase = false, NameContains = "Fake", SearchFor = "PureSearch", TextThatFollows = "SearchArguments", TextThatDoesNotFollow = "" };
             var searchCommand = new SearchCommand(searchFolder, searchArguments);
             var context = new FakeApplicationCommandExecutionContext();
@@ -52,7 +52,7 @@ namespace Aspenlaub.Net.GitHub.CSharp.PureSearch.Test {
 
         [TestMethod]
         public async Task CanSearchOnNotFollowingLine() {
-            var searchFolder = new FakeSearchFolder() { SearchInFolder = vCSharpFolder.FullName + @"\PureSearch\Test\" };
+            var searchFolder = new FakeSearchFolder() { SearchInFolder = vGitHubFolder.FullName + @"\PureSearch\src\Test\" };
             var searchArguments = new FakeSearchArguments() { MatchCase = false, NameContains = "Fake", SearchFor = "PureSearch", TextThatFollows = "", TextThatDoesNotFollow = "SearchArguments" };
             var searchCommand = new SearchCommand(searchFolder, searchArguments);
             var context = new FakeApplicationCommandExecutionContext();
